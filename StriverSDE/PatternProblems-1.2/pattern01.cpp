099cc0321128ab45aebc10ec923cc338bfef189f
satanveer <satanveersingh@gmail.com>
2023-12-22 19:49:12 +0530

    new cpp practice

---
+++ StriverSDE/PatternProblems-1.2/pattern01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for(int i=0;i<4;i++){
+        cout<<endl;
+        for (int j = 0; j < 4; j++)
+        {
+            cout<<"*";
+        }
+        
+    }
+}
+
+//****
+//****
+//****
+//****
